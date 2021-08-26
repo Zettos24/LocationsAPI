aec89720c974451cb3d89cc99c09dd41bbc996a1
Zettos24 <zettos24@gmail.com>
2021-08-26 21:43:50 +0200

    First Commit

---
+++ CONFIG/src/main/java/de/zettos/locationsapi/LocationsAPI.java
@@ -0,0 +1,17 @@
+package de.zettos.locationsapi;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class LocationsAPI extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
